17c9e4d988320ddb2a3fde565148c084ab7e9594
xeniaBch <kseniabekhtgold@gmail.com>
2022-10-27 12:30:05 +0200

    HW tasks

--- src/com/telran/tasks/Task2.java
+++ src/com/telran/tasks/Task2.java
@@ -6,21 +6,24 @@ import java.util.Arrays;
 public class Task2 {
     public static void main(String[] args) {
         double[] array = {0.0, 0.5, 0.6, 0.7, 1.1, 1.4};
-        double sectionLength = 0.4;
+        double sectionLength = 1.4;
         System.out.println(maxNumbersInSection(array, sectionLength));
     }
 
-    public static int maxNumbersInSection(double[] array, double length){
-        int[] count = new int[array.length];
+    public static int maxNumbersInSection(double[] array, double length) {
+        int max = 0;
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++){
-              if(array[j] >= array[i] && array[j] <= array[i] + length)
-                    count[i]++;
+            count = 0;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] >= array[i] && array[j] <= array[i] + length) {
+                    count++;
                 }
             }
-        int max = count[0];
-        for (int i = 1; i < count.length; i++) max = Math.max(max, count[i]);
+            max = Math.max(max, count);
+        }
         return max;
     }
+
 
 }
